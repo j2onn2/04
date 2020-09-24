ffa46dca1ef5df67fd70b4f3061670c08c4a3523
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-09-24 15:49:48 +0900

    practice 2

--- main.c
+++ main.c
@@ -5,19 +5,21 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x, y, z, m ;
-	int a, b, c ;
+	int x, y ;
 	
-	x = 1 ;
-	a = 3 ;
-	b = 4 ;
-	c = 5 ;
-	z = 2 ;
+	printf("input two integers : ");
+	scanf("%i %i", &x, &y) ;
 	
-	y = a*x*x + b*x + c ;
-	m = (x + y + z)/3 ;
+	printf("+ result is %i\n", x+y) ;
 	
-	printf(" y = %d, m = %d", y, m) ;
+	printf("- result is %i\n", x-y) ;
+	
+	printf("* result is %i\n", x*y) ;
+	
+	printf("/ result is %i\n", x/y) ;
+	
+	printf("%% result is %i\n", x%y) ;
+	
 	
 	return 0;
 }
